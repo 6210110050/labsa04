a8d3bba11c6f29630d26c75eee36d9b95e331a17
Jatinut Satsara <you@example.com>
2021-07-12 12:26:46 +0700

    connect

--- components/Weather.js
+++ components/Weather.js
@@ -1,10 +1,28 @@
 import React from 'react';
 import { Text,ImageBackground,StyleSheet } from 'react-native';
 import Forecast from './Forecast';
+import { useEffect } from 'react';
 
 import { useState } from 'react';
 
 export default function Weather(props) {
+    useEffect(() => {
+        console.log(`fetching data with zipCode = ${props.zipCode}`)
+        if (props.zipCode) {
+           fetch(`http://api.openweathermap.org/data/2.5/weather?q=${props.zipCode},th&units=metric&APPID=4728aced2e3fc0f473c435b56ddf1c54`)
+             .then((response) => response.json())
+             .then((json) => {
+                setForecastInfo({
+                   main: json.weather[0].main,
+                   description: json.weather[0].description,
+                   temp: json.main.temp
+        });
+    })
+        .catch((error) => {
+            console.warn(error);
+    });
+}
+        }, [props.zipCode]) 
     const [forecastInfo, setForecastInfo] = useState({
         main: '-',
         description: '-',
@@ -29,6 +47,7 @@ const styles = StyleSheet.create({
         justifyContent:'center',
         alignItems:'center',
         width: '100%',
-        height: '100%'
+        height: '100%',
+        //fontSize:'20px'
     },
 })
